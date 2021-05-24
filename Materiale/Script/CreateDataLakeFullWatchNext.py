eccade72bc9d41aeed0661e2cef578dfbc62f92e
barcigabri <33417277+barcigabri@users.noreply.github.com>
2021-05-24 18:13:52 +0200

    Aggiornati i link e aggiornato nome collection

--- Materiale/Script/CreateDataLakeFullWatchNext.py
+++ Materiale/Script/CreateDataLakeFullWatchNext.py
@@ -16,7 +16,9 @@ from awsglue.job import Job
 
 
 ##### FROM FILES
-tedx_dataset_path = "s3://bucket-dati/tedx_dataset_complete.csv"
+tedx_dataset_path = "s3://unibg-data-2021-bordogna-filippo/tedx_dataset_complete.csv"
+#barce#tedx_dataset_path = "s3://bucket-dati/tedx_dataset_complete.csv"
+#cassi#tedx_dataset_path = "s3://unibg-tcm-lab/tedx_dataset_complete.csv"
 
 ###### READ PARAMETERS
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -53,15 +55,19 @@ print(f"Number of items from RAW DATA {count_items}")
 print(f"Number of items from RAW DATA with NOT NULL KEY {count_items_null}")
 
 
-
 ## READ TAGS DATASET
-tags_dataset_path = "s3://bucket-dati/tags_dataset.csv"
+tags_dataset_path = "s3://unibg-data-2021-bordogna-filippo/tags_dataset.csv"
+#barce# tags_dataset_path = "s3://bucket-dati/tags_dataset.csv"
+#cassi# tags_dataset_path = "s3://unibg-tcm-lab/tags_dataset.csv"
+
 tags_dataset = spark.read.option("header","true").csv(tags_dataset_path)
 
 ## READ WATCH NEXT DATASET
-watch_next_dataset_path = "s3://bucket-dati/watch_next_dataset.csv"
+watch_next_dataset_path = "s3://unibg-data-2021-bordogna-filippo/watch_next_dataset.csv"
+#barce# watch_next_dataset_path = "s3://bucket-dati/watch_next_dataset.csv"
+#cassi# watch_next_dataset_path = "s3://unibg-tcm-lab/watch_next_dataset.csv
+
 watch_next_dataset = spark.read.option("header","true").csv(watch_next_dataset_path)
-######################### "s3://bucket-dati/watch_next_dataset.csv"
 
 
 # CREATE THE AGGREGATE MODEL, ADD TAGS TO TEDX_DATASET
@@ -93,12 +99,14 @@ tedx_dataset_agg_f.printSchema()
 
 
 
-mongo_uri = "mongodb://tcm-shard-00-00.d0u9l.mongodb.net:27017,tcm-shard-00-01.d0u9l.mongodb.net:27017,tcm-shard-00-02.d0u9l.mongodb.net:27017"
+mongo_uri = "mongodb://clustertcm-shard-00-00.nvexe.mongodb.net:27017,clustertcm-shard-00-01.nvexe.mongodb.net:27017,clustertcm-shard-00-02.nvexe.mongodb.net:27017"
+#barce# mongo_uri = "mongodb://tcm-shard-00-00.d0u9l.mongodb.net:27017,tcm-shard-00-01.d0u9l.mongodb.net:27017,tcm-shard-00-02.d0u9l.mongodb.net:27017"
+#cassi# mongo_uri = "mongodb://mycluster-shard-00-00.r8cy1.mongodb.net:27017,mycluster-shard-00-01.r8cy1.mongodb.net:27017,mycluster-shard-00-02.r8cy1.mongodb.net:27017"
 
 write_mongo_options = {
     "uri": mongo_uri,
     "database": "unibg_tedx_2021",
-    "collection": "prova",
+    "collection": "TopTedDB",
     "username": "admin",
     "password": "admin",
     "ssl": "true",
